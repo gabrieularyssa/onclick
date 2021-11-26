83ee0331a92098857c91b44ea7e9f536444ad18f
Gabriela Laryssa <gabrielaoliveiracet@gmail.com>
2021-11-26 10:09:17 -0300

    versao um

---
+++ script.js
@@ -0,0 +1,20 @@
+    function resultado() {
+
+    let Nigirizushi = document.getElementById("notaNigirizushi").value;
+    document.getElementById("resultadoum").innerHTML =  "   " + Nigirizushi;
+
+    let Makizushi = document.getElementById("notaMakizushi").value;
+    document.getElementById("resultadodois").innerHTML =  "   " + Makizushi;
+
+    let Tekkamaki = document.getElementById("notaTekkamaki").value;
+    document.getElementById("resultadotres").innerHTML =  "   " + Tekkamaki;
+
+    let Uramaki = document.getElementById("notaUramaki").value;
+    document.getElementById("resultadoquatro").innerHTML =  "   " + Uramaki;
+
+    let Temaki = document.getElementById("notaTemaki").value;
+    document.getElementById("resultadocinco").innerHTML =  "   " + Temaki;
+    }
+
+    
+   
